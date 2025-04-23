3c7bbafb79068c8d3b0777903ad40aaef40b0b12
Simon Cardona <cdna.simon@gmail.com>
2025-04-23 09:52:24 +0200

    feat(basic-calculator/src): basic functionnality

--- basic-calculator/src/main.c
+++ basic-calculator/src/main.c
@@ -1,19 +1,66 @@
+#include "math.c"
 #include <stdio.h>
+
+enum Operator {
+	ADD = '+',
+	SUB = '-',
+	DIV = '/',
+	MUL = '*',
+};
 
 int main(void){
 	int a, b;
+	int input;
   char operator;
   int total;
 
-  printf("Entrez le premier nombre: ");
-  scanf("%d", &a);
 
-  printf("Entrez l'opérateur (+, -, *, /): ");
-  scanf(" %c", &operator);  // le petit espace avant %c est important !
+	// 1st Step
+	printf("Enter a number: ");
+	input = scanf("%d", &a);
 
-  printf("Entrez le deuxième nombre: ");
-  scanf("%d", &b);
+	if(input != 1 ){
+		printf("Error: This is not a valid integer.\n");
+		return 1;
+	}
+
+	// 2nd Step
+	printf("Choose your operator (+, -, /, *): ");
+	scanf(" %c", &operator);
+
+	if(operator != ADD && operator != SUB && operator != DIV && operator != MUL){
+		printf("Error: %c is not a valid operator.\n", operator);
+		return 1;
+	} 
+
+	// 3rd Step
+	printf("Enter your second number: ");
+	input = scanf("%d", &b);
+
+	if(input != 1){
+		printf("Error: This is not a valid integer.\n");
+		return 1;
+	}
+
+	// 4th Step
+	switch(operator){
+		case ADD:
+			total = add(a, b);
+			break;
+		case SUB:
+			total = substract(a, b);
+			break;
+		case DIV:
+			total = divide(a, b);
+			break;
+		case MUL:
+			total = multiply(a, b);
+			break;
+		default:
+			printf("Sorry this case doesn't work.\n");
+	}
 
 	printf("Total: %d\n", total);
+
 	return 0;
 }
